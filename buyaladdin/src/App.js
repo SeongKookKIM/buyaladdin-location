6f8a74a92dcf9db109a079e016d5a5926c02b462
SeongkookKIM <ksk8646@naver.com>
2023-09-14 15:01:54 +0900

    모바일 화면에 딱 맞는 100vh설정

--- buyaladdin/src/App.js
+++ buyaladdin/src/App.js
@@ -2,8 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import List from "./pages/List";
 import Detial from "./pages/Detial";
+import { useEffect } from "react";
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <div className="App">
       <Routes>
